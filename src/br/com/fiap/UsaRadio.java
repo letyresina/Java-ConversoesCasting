3e20ef4919345cbde86086a8df10757100f7693d
letyresina <leticiaresina@gmail.com>
2024-04-18 11:59:24 -0300

    feito o switch case

--- src/br/com/fiap/UsaRadio.java
+++ src/br/com/fiap/UsaRadio.java
@@ -22,6 +22,38 @@ public class UsaRadio {
 			JOptionPane.showMessageDialog(null, "Estacao "
 					+ radio1.getEstacao() + "\nVolume " + radio1.getVolume());
 			
+			aux = JOptionPane.showInputDialog("Escolha uma das opções abaixo"
+					+ "\n 1 - Aumentar Volume;"
+					+ "\n 2 - Diminuir Volume;"
+					+ "\n 3 - Trocar de estação");
+			
+			escolha = Integer.parseInt(aux);
+			
+			switch (escolha) {
+			case 1: {
+				radio1.aumentarVolume();
+				JOptionPane.showMessageDialog(null, "Estacao "
+						+ radio1.getEstacao() + "\nVolume " + radio1.getVolume());
+				break;
+			}
+			case 2: {
+				radio1.diminuirVolume();
+				JOptionPane.showMessageDialog(null, "Estacao "
+						+ radio1.getEstacao() + "\nVolume " + radio1.getVolume());
+				break;
+			}
+			case 3: {
+				aux = JOptionPane.showInputDialog("Escolha a nova estação");
+				estacao = Float.parseFloat(aux);
+				radio1.setEstacao(estacao);
+				JOptionPane.showMessageDialog(null, "Estacao "
+						+ radio1.getEstacao() + "\nVolume " + radio1.getVolume());
+				break;
+			}
+			default:
+				throw new Exception("Opção inválida");
+			}
+			
 		} catch (Exception e) {
 			System.out.println("Erro: " + e.getMessage());
 		}
